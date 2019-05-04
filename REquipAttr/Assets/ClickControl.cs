a8cb9b618dc74fadfad43a38ecf0475ca52461e6
caodi789 <100061501@qq.com>
2019-05-05 04:28:22 +0800

    Equip View

---
+++ REquipAttr/Assets/ClickControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickControl : MonoBehaviour
+{
+    GameObject Bag;
+
+
+    public void EquipClick()
+    {
+        Bag.SetActive(!Bag.activeSelf);
+    }
+
+    private void Start()
+    {
+        Bag = GameObject.FindGameObjectWithTag("Bag");
+        Bag.SetActive(!Bag.activeSelf);
+    }
+}
---
+++ REquipAttr/Assets/TopBarEvent.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TopBarEvent : MonoBehaviour
+{
+    public Transform toolbar;
+    Vector3 m_OPos;
+
+    IEnumerator Temp;
+
+    private void Start()
+    {
+        m_OPos = toolbar.localPosition;
+        
+    }
+
+
+    public void MouseEnter(BaseEventData e)
+    {
+        Temp = Move();
+        if (toolbar.localPosition == m_OPos)
+        {
+            StartCoroutine(Temp);
+           
+        }
+
+        Debug.Log("进入");
+    }
+    public void MouseExit(BaseEventData e)
+    {
+        StopCoroutine(Temp);
+        toolbar.localPosition = m_OPos;
+
+        Debug.Log("离开");
+    }
+
+    IEnumerator Move()
+    {
+        // 1= 1000ms
+        var mSeconds = 0.1f;
+        var number = 60.0f * mSeconds;
+        var mFrames = 1.0f / 60.0f;
+        for (int i = 0; i < number; i++)
+        {
+            toolbar.localPosition += new Vector3(0, -30.0f/ number , 0);
+            
+            yield return new WaitForSeconds(mFrames);
+        }
+    }
+}
